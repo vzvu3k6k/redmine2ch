eebdc763b0cc044b99f1e8c901e45e0b2b16132b
vzvu3k6k <vzvu3k6k@gmail.com>
2019-11-22 02:23:49 +0900

    Update bbs.rb

--- lib/bbs.rb
+++ lib/bbs.rb
@@ -4,13 +4,21 @@ require 'sinatra/base'
 require 'datpot/board'
 
 class Bbs < Sinatra::Application
-  get '/redmine/subject.txt' do
-    content_type 'plain/text'
-    subject_txt.encode('cp932')
+  def subject_txt(_board_id)
+    raise NotImplementedError
   end
 
-  get '/redmine/dat/1234.dat' do
+  def dat(_board_id, _dat_id)
+    raise NotImplementedError
+  end
+
+  get '/:board_id/subject.txt' do
     content_type 'plain/text'
-    dat.encode('cp932')
+    subject_txt(params['board_id']).encode('cp932')
+  end
+
+  get '/:board_id/dat/:dat_id.dat' do
+    content_type 'plain/text'
+    dat(params['board_id'], params['dat_id']).encode('cp932')
   end
 end
